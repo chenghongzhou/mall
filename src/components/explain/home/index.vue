c479b81f14362ad1943a621f9c9e1071af022b12
hy-PC\hy <529622672@qq.com>
2020-05-18 10:02:52 +0800

    1

--- src/components/explain/home/index.vue
+++ src/components/explain/home/index.vue
@@ -224,7 +224,7 @@ export default {
                 pagination: ".swiper-p2",
                 autoplayDisableOnInteraction : false,
                 onSlideChangeEnd(swiper){
-
+                    
                 }
             });
        },
