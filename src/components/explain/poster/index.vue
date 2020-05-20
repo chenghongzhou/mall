d25958dccf033620ba5fe0e87afa72ac750842f2
hy-PC\hy <529622672@qq.com>
2020-05-20 17:46:33 +0800

    1

--- src/components/explain/poster/index.vue
+++ src/components/explain/poster/index.vue
@@ -1,48 +1,85 @@
 <template>
     <div class="main">
-        <div id="posterHtml">
-            <div class="edit">
-                <div style="position:relative;width:100%;height:100%">
-                    <img src="../../../../static/images/home/my_img.png" alt="" class="my_img">
-                    <img src="../../../../static/images/home/wx_code.jpg" alt="" class="code_img">
-                    <div class="intru">关注作者公众号获取更多福利</div>
+        <!-- 要生成海报的-->
+      <div id="posterHtml"  ref="creatGivePoster" >
+         <div class="edit">
+            <div style="position:relative;width:100%;height:100%">
+                <img src="../../../../static/images/home/my_img.png" alt="" class="my_img">
+                <div class="code_img">
+                    <vue-qr :logo-src="config.logo" :logoScale="0.25" :text="config.value" :margin="0" style="width:85%;height:85%;display:block"  class="player-bottom-panel-qrcode center" />
                 </div>
+                
+                <div class="intru">关注作者公众号获取更多福利</div>
             </div>
         </div>
+      </div>
+        <!-- 生成海报的-->
+      <div class="share-give-poster">
+        <img class="poster-img"  :src="imgUrl" v-if="isImg">
+      </div>
     </div>
 </template>
 
 <script>
-import QRCode from 'qrcodejs2';
+import vueQr from 'vue-qr';
 import html2canvas from 'html2canvas';
 export default {
+    components: {
+        vueQr
+    },
     data(){
         return {
-
+            config: {
+                value: window.location.href, //显示的值、跳转的地址
+                logo: require('../../../../static/images/home/logo.png') //中间logo的地址
+            },
+             imgUrl: '', //最后转化出来的图片base64地址
+             isImg: false
         }
     },
-    methods: {
-        createPoster() {
-        // 生成海报
-            const vm = this
-            const domObj = document.getElementById('posterHtml')
-            html2canvas(domObj, {
-                useCORS: true,
-                allowTaint: false,
-                logging: false,
-                letterRendering: true,
-                onclone(doc) {
-                let e = doc.querySelector('#posterHtml')
-                e.style.display = 'block'
-                }
-            }).then(function(canvas) {
-                // 在微信里,可长按保存或转发
-                vm.posterImg = canvas.toDataURL('image/png')
+      methods: {
+    // 生成海报
+        getPoster() {
+            var _this = this;
+            let shareContent = this.$refs.creatGivePoster //需要截图的包裹的（原生的）DOM 对象
+            let width = shareContent.offsetWidth //获取dom 宽度
+            let height = shareContent.offsetHeight //获取dom 高度
+            let canvas = document.createElement('canvas') //创建一个canvas节点
+            let scale = 2 //定义任意放大倍数 支持小数
+            canvas.width = width * scale //定义canvas 宽度 * 缩放
+            canvas.height = height * scale //定义canvas高度 *缩放
+            canvas.getContext('2d').scale(scale, scale) //获取context,设置scale
+            let opts = {
+                scale: 1, // 添加的scale 参数
+                canvas: canvas, //自定义 canvas
+                // logging: true, //日志开关，便于查看html2canvas的内部执行流程
+                width: width, //dom 原始宽度
+                height: height,
+                useCORS: true, // 【重要】开启跨域配置
+                backgroundColor: null,
+                dpi:96
+            }
+
+            html2canvas(shareContent, opts).then(canvas => {
+                var context = canvas.getContext('2d')
+                let shareContent = _this.$refs.creatGivePoster //需要截图的包裹的（原生的）DOM 对象
+                let width = shareContent.offsetWidth //获取dom 宽度
+                let height = shareContent.offsetHeight //获取dom 高度
+                // 【重要】关闭抗锯齿
+                context.mozImageSmoothingEnabled = false
+                context.webkitImageSmoothingEnabled = false
+                context.msImageSmoothingEnabled = false
+                context.imageSmoothingEnabled = false
+                // 【重要】默认转化的格式为png,也可设置为其他格式
+                let imgUrl = canvas.toDataURL('image/png')
+                this.imgUrl = imgUrl
+                this.isImg = true
+                console.log('生成海报')
             })
-        },
+        }
     },
     mounted(){
-        this.createPoster();
+        setTimeout(_ => { this.getPoster() }, 500)
     }
 }
 </script>
@@ -97,5 +134,19 @@ export default {
         position: absolute;
         bottom: 0.87rem;
         left: 3rem;
+    }
+    .share-give-poster{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+    .share-give-poster img{
+        width: 100%;
+        height: 100%;
+    }
+    .code_img img{
+        margin: 7.5% auto;
     }
 </style>
